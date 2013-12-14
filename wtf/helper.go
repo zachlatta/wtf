5a3a45bfdc8cae8d44633c20c5856a4375f52fa7
Zach Latta <zach@zachlatta.com>
2013-12-14 01:56:20 -0800

    Moving doge.

---
+++ wtf/helper.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "errors"
+  "image"
+  "image/png"
+  "io"
+
+  "github.com/go-gl/gl"
+)
+
+func createTexture(r io.Reader) (gl.Texture, error) {
+  img, err := png.Decode(r)
+  if err != nil {
+    return gl.Texture(0), err
+  }
+
+  rgbaImg, ok := img.(*image.NRGBA)
+  if !ok {
+    return gl.Texture(0), errors.New("texture must be an NRGBA image")
+  }
+
+  textureId := gl.GenTexture()
+  textureId.Bind(gl.TEXTURE_2D)
+  gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+  gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+
+  // flip image: first pixel is lower left corner
+  imgWidth, imgHeight := img.Bounds().Dx(), img.Bounds().Dy()
+  data := make([]byte, imgWidth*imgHeight*4)
+  lineLen := imgWidth * 4
+  dest := len(data) - lineLen
+  for src := 0; src < len(rgbaImg.Pix); src += rgbaImg.Stride {
+    copy(data[dest:dest+lineLen], rgbaImg.Pix[src:src+rgbaImg.Stride])
+    dest -= lineLen
+  }
+  gl.TexImage2D(gl.TEXTURE_2D, 0, 4, imgWidth, imgHeight, 0, gl.RGBA,
+    gl.UNSIGNED_BYTE, data)
+
+  return textureId, nil
+}
---
+++ wtf/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+  "fmt"
+  "os"
+
+  "github.com/go-gl/gl"
+  glfw "github.com/go-gl/glfw3"
+)
+
+const (
+  Title string = "Where's the Refrigerator?"
+  Width, Height int = 640, 480
+)
+
+var texture gl.Texture
+
+func errorCallback(err glfw.ErrorCode, desc string) {
+  fmt.Printf("%v: %v\n", err, desc)
+}
+
+func main() {
+  glfw.SetErrorCallback(errorCallback)
+
+  if !glfw.Init() {
+    panic("Can't init glfw!")
+  }
+  defer glfw.Terminate()
+
+  window, err := glfw.CreateWindow(Width, Height, Title, nil, nil)
+  if err != nil {
+    panic(err)
+  }
+
+  window.MakeContextCurrent()
+  glfw.SwapInterval(1)
+
+  gl.Init()
+
+  if err := initScene(); err != nil {
+    fmt.Fprintf(os.Stderr, "init: %s\n", err)
+    return
+  }
+  defer destroyScene()
+
+  for !window.ShouldClose() {
+    draw(window)
+    window.SwapBuffers()
+    glfw.PollEvents()
+  }
+}
+
+func initScene() (err error) {
+  gl.Enable(gl.TEXTURE_2D)
+
+  gl.ClearColor(0.0, 0.0, 0.0, 0.0)
+
+  gl.Viewport(0, 0, Width, Height)
+  gl.Ortho(0, float64(Width), float64(Height), 0, -10, 10)
+  gl.MatrixMode(gl.PROJECTION)
+  gl.LoadIdentity()
+
+  doge, err := os.Open("doge-freezer.png")
+  if err != nil {
+    panic(err)
+  }
+  defer doge.Close()
+
+  texture, err = createTexture(doge)
+  return
+}
+
+func destroyScene() {
+  texture.Delete()
+}
+
+var x, y float32 = 0, 0
+var w, h float32 = 526.0, 526.0
+
+func draw(window *glfw.Window) {
+  gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+  gl.PushMatrix()
+  gl.Translatef(x, y, 0)
+
+  texture.Bind(gl.TEXTURE_2D)
+
+  gl.Begin(gl.QUADS)
+  gl.Color4f(1, 1, 1, 1)
+  gl.TexCoord2f(1, 1)
+  gl.Vertex2f(0, 1)
+  gl.TexCoord2f(1, 0)
+  gl.Vertex2f(0, h)
+  gl.TexCoord2f(0, 0)
+  gl.Vertex2f(w, h)
+  gl.TexCoord2f(0, 1)
+  gl.Vertex2f(w, 0)
+  gl.TexCoord2f(0, 1)
+  gl.Vertex2f(w, 0)
+  gl.End()
+
+  gl.PopMatrix()
+
+  increment := float32(0.005)
+  if window.GetKey(glfw.KeyW) == glfw.Press {
+    y += increment
+  }
+  if window.GetKey(glfw.KeyA) == glfw.Press {
+    x -= increment
+  }
+  if window.GetKey(glfw.KeyS) == glfw.Press {
+    y -= increment
+  }
+  if window.GetKey(glfw.KeyD) == glfw.Press {
+    x += increment
+  }
+}
